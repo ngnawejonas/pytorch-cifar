e20eec84d692cb0c80410e39064f8616980b4020
Jonas Ngnawe <ngnawejonas@gmail.com>
2022-09-04 12:05:39 -0400

    data load fix

--- data.py
+++ data.py
@@ -117,41 +117,6 @@ def get_FashionMNIST(handler, pool_size):
     return get_xMNIST(datasets.FashionMNIST, handler, pool_size, 'Fashion')
 
 
-def get_SVHN(handler, pool_size):
-    transform_train = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4377, 0.4438, 0.4728), (0.1980, 0.2010, 0.1970)),
-    ])
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4377, 0.4438, 0.4728), (0.1980, 0.2010, 0.1970)),
-    ])
-    data_train = datasets.SVHN('data', split='train', download=True, transform=transform_train)
-    data_test = datasets.SVHN('data', split='test', download=True, transform=transform_test)
-
-    dtl = DataLoader(data_train, batch_size=len(data_train))
-    for X,y in dtl:
-        X_train = X
-        Y_train = y
-
-    dtl = DataLoader(data_test, batch_size=len(data_test))
-    for X,y in dtl:
-        X_test = X
-        Y_test = y
-
-    # print('data.py:146 ', X_train.data.shape, X_train.dtype, type(X_train))
-    return Data(X_train, Y_train, X_test, Y_test, handler)
-
-# def get_CIFAR10(handler, pool_size):
-#     data_train = datasets.CIFAR10('./data/CIFAR10', train=True, download=True)
-#     data_test = datasets.CIFAR10('./data/CIFAR10', train=False, download=True)
-#     print('data.py:146 ', data_train.data.shape)
-#     return Data(data_train.data[:pool_size],
-#                 torch.LongTensor(data_train.targets)[:pool_size],
-#                 data_test.data[:pool_size],
-#                 torch.LongTensor(data_test.targets)[:pool_size],
-#                 handler)
-
 def get_CIFAR10(handler, pool_size, n_adv_test):
     transform_train = transforms.Compose(
             [
