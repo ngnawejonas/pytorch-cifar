3968ca2c4c50771783d94a861b8a89e17564e4da
Jonas Ngnawe <ngnawejonas@gmail.com>
2022-09-04 20:33:40 -0400

    test handler

--- handlers.py
+++ handlers.py
@@ -22,33 +22,37 @@ class MNIST_Handler(Dataset):
     def __len__(self):
         return len(self.X)
 
-class SVHN_Handler(Dataset):
-    def __init__(self, X, Y):
-        self.X = X
-        self.Y = Y
-        # self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4377, 0.4438, 0.4728), (0.1980, 0.2010, 0.1970))])
-
-    def __getitem__(self, index):
-        x, y = self.X[index], self.Y[index]
-        # x = Image.fromarray(np.transpose(x, (1, 2, 0)))
-        # x = self.transform(x)
-        return x, y, index
-
-    def __len__(self):
-        return len(self.X)
 
 # https://github.com/kuangliu/pytorch-cifar/blob/master/main.py
 class CIFAR10_Handler(Dataset):
-    def __init__(self, X, Y):
+    def __init__(self, X, Y, train=True):
         self.X = X
         self.Y = Y
-        # self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))])
-
+        if train:
+            self.transform = transforms.Compose(
+                    [
+                        transforms.RandomCrop(32, padding=4),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.ToTensor(),
+                        transforms.Normalize(
+                            (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+                        ),
+                    ]
+                )
+        else:
+            self.transform = transforms.Compose(
+                [
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+                    ),
+                ]
+            )
 
     def __getitem__(self, index):
         x, y = self.X[index], self.Y[index]
-        # x = Image.fromarray(x)
-        # x = self.transform(x)
+        x = Image.fromarray(x)
+        x = self.transform(x)
         return x, y, index
 
     def __len__(self):
